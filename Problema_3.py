a6d94c80cf19eab1d1aa3443bb5454539754b60e
Maxim Durnea <70955732+BarSick69@users.noreply.github.com>
2020-09-19 23:43:32 +0300

    Update Problema_3.py

--- Problema_3.py
+++ Problema_3.py
@@ -1,4 +1,4 @@
 L = int(input("lungimea laturii = "))
 print("perimetrul: ", 4*L, "cm")
-print("volumul: ", L**3, "cm")
-print("aria tuturor suprafetelor: ", 6*(L**2), "cm")
+print("volumul: ", L**3, "cm3")
+print("aria tuturor suprafetelor: ", 6*(L**2), "cm2")
